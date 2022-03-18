45e23d9a4717d2baf6b80550c741915ac52b8d1f
your Name <beimar.jalvin@vivaair.com>
2022-03-18 14:45:07 -0500

    add compone y server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './component/book/book.component';
+import { BookFormComponent } from './component/book-form/book-form.component';
+import { BookListComponent } from './component/book-list/book-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    BookFormComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
